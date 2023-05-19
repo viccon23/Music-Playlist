deccd80d9c1fc35bf12db9cf294d633c6bf9ff46
Victor Contreras <123285276+viccon23@users.noreply.github.com>
2023-05-18 19:38:28 -0500

    Update musicPlaylist.cpp

--- musicPlaylist.cpp
+++ musicPlaylist.cpp
@@ -6,21 +6,6 @@ using namespace std;
 
 #include "code.h"
 
-void displayMenu() {
-  cout << "Welcome to C++ Playlist! Choose an Option Below:" << endl;
-  cout << "======================================" << endl;
-  cout << "     1.  Create Playlist" << endl;
-  cout << "     2.  Add track to playlist" << endl;
-  cout << "     3.  Remove the first track of playlist" << endl;
-  cout << "     4.  Remove track by song title" << endl;
-  cout << "     5.  Toggle a track as favorited" << endl;
-  cout << "     6.  Insert track at a specific position" << endl;
-  cout << "     7.  Display Playlist" << endl;
-  cout << "     8.  Search Playlist by genre" << endl;
-  cout << "     9.  Exit" << endl;
-  cout << "======================================" << endl;
-  cout << "Enter your choice: ";
-}
 int main() {
 
   int choice = 0;
@@ -37,7 +22,7 @@ int main() {
     
     switch (choice) {
     case 1: {
-      cout << "Choose Playlist Name" << endl;
+      cout << "\nChoose Playlist Name: \n" << endl;
       string Title;
       getline(cin, Title);
       // Assign the Playlist object to the declared variable
@@ -47,27 +32,27 @@ int main() {
 
     case 2: {
       // Create the new song object
-      cout << "Let's Add a track to our playlist!" << endl;
-      cout << "Input the name of the track: " << endl;
+      cout << "\n\nLet's Add a track to our playlist!" << endl;
+      cout << "Input the name of the track: \n" << endl;
       string t, al, len;
       vector<string> a;
       vector<string> g;
       getline(cin, t);
-      cout << "Input artist names (Enter 'done' to finish):" << endl;
+      cout << "\nInput artist names (Enter 'done' to finish):\n" << endl;
       string artist;
       while (getline(cin, artist) && artist != "done") {
         a.push_back(artist);
       }
-      cout << "Input Genres of the song (enter 'done' to finish):" << endl;
+      cout << "\nInput Genres of the song (enter 'done' to finish):\n" << endl;
       string genre;
       while (getline(cin, genre) && genre != "done") {
         g.push_back(genre);
       }
-      cout << "Input name of Album where the song is included. If it's not in "
-              "an album, type N/A: "
+      cout << "\nInput name of Album where the song is included. If it's not in "
+              "an album, type N/A: \n"
            << endl;
       getline(cin, al);
-      cout << "Type the length of the song in minute:second format:" << endl;
+      cout << "\nType the length of the song in minute:second format:\n" << endl;
       getline(cin, len);
       Song *song = new Song(t, a, g, al, len);
       // Finally, add song object to the playlist
@@ -76,13 +61,14 @@ int main() {
     }
 
     case 3: {
-      cout << "Removing first track in playlist" << endl;
+      cout << "\nRemoving first track in playlist....\n" << endl;
       playlist.removeFirstTrack();
+      cout << "Track Removed!\n";
       break;
     }
     case 4: {
-      cout << "Enter the name of the track you want to remove from your "
-              "playlist: "
+      cout << "\nEnter the name of the track you want to remove from your "
+              "playlist: \n"
            << endl;
       string title;
       getline(cin, title);
@@ -91,7 +77,7 @@ int main() {
       break;
     }
     case 5: {
-      cout << "Enter the name of the track you want to playlist" << endl;
+      cout << "\nEnter the name of the track you want to playlist\n" << endl;
       string title;
       getline(cin, title);
       playlist.toggleFavorite(title);
@@ -99,33 +85,33 @@ int main() {
       break;
     }
     case 6: {
-      cout << "Let's Add a track to our playlist!" << endl;
-      cout << "Input the name of the track: " << endl;
+      cout << "\nLet's Add a track to our playlist!" << endl;
+      cout << "Input the name of the track: \n" << endl;
       string t, al, len;
       vector<string> a;
       vector<string> g;
       getline(cin, t);
-      cout << "Input artist names (Enter 'done' to finish):" << endl;
+      cout << "\nInput artist names (Enter 'done' to finish):\n" << endl;
       string artist;
       while (getline(cin, artist) && artist != "done") {
         a.push_back(artist);
       }
-      cout << "Input Genres of the song (enter 'done' to finish):" << endl;
+      cout << "\nInput Genres of the song (enter 'done' to finish):\n" << endl;
       string genre;
       while (getline(cin, genre) && genre != "done") {
         g.push_back(genre);
       }
-      cout << "Input name of Album where the song is included. If it's not in "
-              "an album, type N/A: "
+      cout << "\nInput name of Album where the song is included. If it's not in "
+              "an album, type N/A: \n"
            << endl;
       getline(cin, al);
-      cout << "Type the length of the song in minute:second format:" << endl;
+      cout << "\nType the length of the song in minute:second format:\n" << endl;
       getline(cin, len);
       Song *song = new Song(t, a, g, al, len);
-      cout << "What position do you want to insert your track at?\nNote: If "
+      cout << "\nWhat position do you want to insert your track at?\nNote: If "
               "you input a number larger than the playlist, then the track "
               "will be placed at the end. If the number is 0, the track will "
-              "be placed in the beginning. No negative values are allowed."
+              "be placed in the beginning. No negative values are allowed.\n"
            << endl;
       int position;
       cin >> position;
@@ -134,12 +120,12 @@ int main() {
       break;
     }
     case 7: {
-      cout << "Here are all the songs in the playlist" << endl;
+      cout << "\nHere are all the songs in the playlist\n" << endl;
       playlist.displayPlaylist();
       break;
     }
     case 8: {
-      cout << "What genre would you like to filter the playlist by?: " << endl;
+      cout << "\nWhat genre would you like to filter the playlist by?: \n" << endl;
       string genre;
       getline(cin, genre);
       playlist.searchByGenre(genre);
@@ -156,35 +142,3 @@ int main() {
   }
   
 }
-
-/* Display the playlist
-myPlaylist.displayPlaylist();
-
-// Toggle a track as a favorite
-myPlaylist.toggleFavorite("Bohemian Rhapsody");
-myPlaylist.toggleFavorite("Ghosts 'n' Stuff");
-myPlaylist.toggleFavorite("Your Mind");
-
-// Display the playlist again to show the favorite track
-myPlaylist.displayPlaylist();
-
-// Search for songs by genre
-myPlaylist.searchByGenre("Rock");
-
-// Remove a track from the playlist
-myPlaylist.removeTrack("I Will Always Love You");
-
-// Display the playlist again to show the removed track
-myPlaylist.displayPlaylist();
-
-// Insert a track at a specific position in the playlist
-myPlaylist.insertTrackAt(s3, {"Pop", "R&B"}, {"Whitney Houston"}, 2);
-
-// Display the playlist again to show the inserted track
-myPlaylist.displayPlaylist();
-
-cout << endl << endl;
-myPlaylist.searchByGenre("Techno");
-
-return 0;
-} */
